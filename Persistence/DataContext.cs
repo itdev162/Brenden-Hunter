f708d3a16c45c8165f05ef627b86bd22dd2785b8
brenden hunter <hunterb7@gmatc.matc.edu>
2021-10-12 12:16:36 -0500

    Update

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+namespace Persistence
+{
+public class DataContext : DbContext
+    {
+        
+        public DbSet<Value> Values { get; set; }
+        
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DataContext(){
+
+        }
+        
+}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Brenden-Hunter
+
+test
