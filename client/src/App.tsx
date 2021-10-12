af4b205f058c1f688294429781d315c520502dc4
brenden hunter <hunterb7@gmatc.matc.edu>
2021-10-12 12:58:34 -0500

    Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+
+class App extends React.Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+    .then((response) =>{
+      this.setState({
+        values: response.data
+      })
+    })
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    })
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        BlogBox
+        </header>
+        {this.state.values.map((value: any) => <div key={value}>{value}</div>)}
+        </div>
   );
+  }
 }
 
 export default App;
