d1f21c6f19467f1a9b780f29ae900937ae0545ee
brenden hunter <hunterb7@gmatc.matc.edu>
2021-10-12 09:59:05 -0500

    Update

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -6,13 +6,14 @@ using Microsoft.EntityFrameworkCore;
 using Domain;
 namespace Persistence
 {
-    public class DataContext : DbContext
+public class DataContext: DbContext
     {
+        
         public DbSet<Value> Values { get; set; }
-
+        
         public DataContext(DbContextOptions options) : base(options)
         {
 
         }
-    }
+        
 }
