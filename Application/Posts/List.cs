2eb0d1ca9d4de25450b59d572b81f95706faf716
brenden hunter <hunterb7@gmatc.matc.edu>
2021-10-31 14:39:43 -0500

    Completed Activity 6

--- Application/Posts/List.cs
+++ Application/Posts/List.cs
@@ -12,19 +12,20 @@ namespace Application.Posts
 {
     public class List
     {
-        
-        public class Query : IRequest<List<Post>>{}
+
+        public class Query : IRequest<List<Post>> { }
 
         public class Handler : IRequestHandler<Query, List<Post>>
         {
             private readonly DataContext context;
-            
+
             public Handler(DataContext context) => this.context = context;
-                
+
             public Task<List<Post>> Handle(Query request, CancellationToken cancellationToken)
             {
                 return this.context.Posts.ToListAsync();
             }
         }
+
     }
 }
